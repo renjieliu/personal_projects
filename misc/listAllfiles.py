5d597b7962d772dac1a06278cd9f21e065787932
renjieliu <anlrj@qq.com>
2021-07-04 18:24:02 -0400

    Add files via upload

--- misc/listAllfiles.py
+++ misc/listAllfiles.py
@@ -1,26 +1,32 @@
+# The script is to list all the files inside a folder on a host, along with the files within the subfolder
 from datetime import datetime
 import os
 
-def listAllFiles(output,hostname, path, curr_time, output_file):
-    arr = os.listdir(path)
-    for a in arr:
-        realPath = path + '/' + a
-        if os.path.isdir(realPath):
-            listAllFiles(output, hostname, realPath, curr_time,output_file)
-        else:
-            output.append(f'{hostname}`{path}/{a}`{curr_time}')
-
-	with open(output_file, 'w', encoding='utf-8') as file:
-		for o in output:
-			s = (o+"\n").replace("\\","\\\\")
-			file.write(s)
-		
-		
 hostname = "T430"
 path = 'D:\\My Files'
 output_file = f"D:\\files.txt"
 curr_time = datetime.now()
 output = []
 
+
+def listAllFiles(output,hostname, path, curr_time):
+    arr = os.listdir(path)
+    for a in arr:
+        realPath = path + '/' + a
+        if os.path.isdir(realPath):
+            output.append(f'{hostname}`{path}/{a}`{curr_time}')
+            listAllFiles(output, hostname, realPath, curr_time)
+        else:
+            output.append(f'{hostname}`{path}/{a}`{curr_time}')
+
 listAllFiles(output, hostname, path, curr_time)
 
+
+# output the result to a file
+with open(output_file, 'w', encoding='utf-8') as file:
+    for o in output:
+        s = (o+"\n").replace("\\","\\\\")
+        file.write(s)
+
+		
+		
