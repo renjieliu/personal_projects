41c4e3a6e61f9561a7cd8ec1e5c5265a6511a918
renjieliu <anlrj@qq.com>
2021-11-30 22:04:57 -0500

    Create python_built_in_class_customize_sample.py

---
+++ misc/python_built_in_class_customize_sample.py
@@ -0,0 +1,16 @@
+# RL added on 2021-11-30
+
+class list: #override the internal class
+    def __init__(self, val):
+        self.val = val
+        print(f"hello list {val}")
+        
+    def __add__(self, b): # operator "+" overloading
+        print("adding..")
+        return self.val * b.val
+
+a = list(100)
+b = list(200)
+
+print(a + b)
+
