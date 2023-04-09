d5cdb536311640b8a2a18e6040b66ce2764b5020
Renjie Liu <anlrj@qq.com>
2023-04-08 21:14:41 -0400

    v

--- misc/python/_draft.py
+++ misc/python/_draft.py
@@ -1,32 +1,41 @@
-import pandas as pd
-import pyodbc
-import os
-import warnings
-import pymssql
-import openpyxl
-import matplotlib.pyplot as plt
-import time
+# import pandas as pd
+# import pyodbc
+# import os
+# import warnings
+# import pymssql
+# import openpyxl
+# import matplotlib.pyplot as plt
+# import time
 
 
-warnings.filterwarnings('ignore') # do not show Python package internal warnings
+# warnings.filterwarnings('ignore') # do not show Python package internal warnings
 
-server = 'server:port'
-db = 'dbName'
-username = 'usr'
-pwd = 'pwd'
-scriptsFolder = r'path_to_sql_folder'
-outputExcel = r'path_to_output_excel_file'
-
-
-#cnxn = pyodbc.connect(f'Driver=SQL Server;Server={server};Database={db};Trusted_Connection=yes;')
-# xxx = pymssql.connect(host=server, database=db, user=username, password=pwd)
+# server = 'server:port'
+# db = 'dbName'
+# username = 'usr'
+# pwd = 'pwd'
+# scriptsFolder = r'path_to_sql_folder'
+# outputExcel = r'path_to_output_excel_file'
 
 
+# #cnxn = pyodbc.connect(f'Driver=SQL Server;Server={server};Database={db};Trusted_Connection=yes;')
+# # xxx = pymssql.connect(host=server, database=db, user=username, password=pwd)
 
-print(pymssql.__file__)
-print(pd.__file__)
 
-print(pymssql.__version__)
+
+# print(pymssql.__file__)
+# print(pd.__file__)
+
+# print(pymssql.__version__)
+
+import sys
+
+
+for i in range(10):  
+    print("Loading" + "." * i) 
+    print("newline, should be covered")
+    sys.stdout.write("\033[F, new line: covering the previous line") # Cursor up one line
+
 
 
 
