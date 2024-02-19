774c80e75107d19558d78f2028b1c0c466dcd5dd
Renjie Liu <anlrj@qq.com>
2024-02-19 17:00:19 -0500

    8 queen.sql

--- misc/SQL/8 queen.sql
+++ misc/SQL/8 queen.sql
@@ -1,36 +1,43 @@
-with pos as (
-select 0 id 
-union all 
-select id + 1  from pos
-where id < 63
+; with pos as ( -- assign number for each grid
+	select id = 0
+	union all 
+	select id+1 
+	from pos 
+	where id < 63
 ) 
-, cte as 
-(
-	select board = cast(replicate('-', 64) as varchar(max))
-		, id = -1 , num = 0 -- base
-		union all
-	select substring(board, 1, p1.id)  + '#' + substring(board, p1.id+2, len(board)-p1.id+1) 
-		, p1.id
-		, num+1 -- iterating
-	from cte, pos p1
-	where num < 8 
-	and p1.id > cte.id -- no need to check the i < current. speed up performance
-	and not exists 
-	(
-		select * from pos ps
-		where 
-		SUBSTRING(cte.board, ps.id+1, 1) = '#' -- current one is taken
+, solution as 
+(select 
+	board = cast(replicate('-', 64) as varchar(max)) -- form the base board
+	, finalID = -1 -- last id to be placed on the board 
+	, number = 0  -- number of queen 
+ union all 
+	select 
+	stuff(board, p.id+1 , 1, '#') --  id starts from 0, add 1 to start from 1st location in the string 
+	, p.id
+	, number+1
+	from solution s , pos p 
+	where 
+	p.id > s.finalID  -- only check the ones after current id, to speed up the search
+	and s.number < 8  
+	and not exists (
+		select * from pos p1 
+		where
+		SUBSTRING(board, p1.id+1, 1) = '#' -- current id is not used 
 		and 
-		(ps.id % 8 = p1.id % 8 -- same col
-		or ps.id/8 = p1.id / 8 -- same row
-		or (ps.id/8) + (ps.id % 8) = (p1.id/8) + (p1.id%8) -- right upper diag
-		or (ps.id/8) - (ps.id % 8) = (p1.id/8) - (p1.id%8) -- left upper diag
-		)
-	)
-)
-select *  from cte
-where num = 8
+		( p1.id / 8 = p.id / 8 -- same row 
+			or p1.id % 8 = p.id % 8  -- same col
+			or p1.id / 8 + p1.id % 8 = p.id / 8 + p.id % 8  -- go right upper diag
+			or p1.id / 8 - p1.id % 8 = p.id / 8 - p.id % 8  -- go left lower diag
+			)
+ )
+ 
+) 
+select * from solution 
+where number = 8 
 
 
 
---select 23 % 8 
+
+
+
+
