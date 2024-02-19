a925e1daf6fff39f48367db79d1d400b353dc734
Renjie Liu <anlrj@qq.com>
2024-02-19 17:00:50 -0500

    8 queen.sql

--- misc/SQL/8 queen.sql
+++ misc/SQL/8 queen.sql
@@ -6,11 +6,11 @@
 	where id < 63
 ) 
 , solution as 
-(select 
+(	select 
 	board = cast(replicate('-', 64) as varchar(max)) -- form the base board
 	, finalID = -1 -- last id to be placed on the board 
 	, number = 0  -- number of queen 
- union all 
+ 	union all 
 	select 
 	stuff(board, p.id+1 , 1, '#') --  id starts from 0, add 1 to start from 1st location in the string 
 	, p.id
@@ -19,7 +19,8 @@
 	where 
 	p.id > s.finalID  -- only check the ones after current id, to speed up the search
 	and s.number < 8  
-	and not exists (
+	and 
+	not exists (
 		select * from pos p1 
 		where
 		SUBSTRING(board, p1.id+1, 1) = '#' -- current id is not used 
@@ -29,7 +30,7 @@
 			or p1.id / 8 + p1.id % 8 = p.id / 8 + p.id % 8  -- go right upper diag
 			or p1.id / 8 - p1.id % 8 = p.id / 8 - p.id % 8  -- go left lower diag
 			)
- )
+ 	)
  
 ) 
 select * from solution 
