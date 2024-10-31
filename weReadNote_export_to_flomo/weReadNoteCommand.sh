22706b54ef0fc2690ccfd238ac083effc6cec1b8
renjieliu <anlrj@qq.com>
2024-10-31 16:27:23 -0400

    Update weReadNoteCommand.sh

--- weReadNote_export_to_flomo/weReadNoteCommand.sh
+++ weReadNote_export_to_flomo/weReadNoteCommand.sh
@@ -1,2 +1,1 @@
 #!/bin/bash
-curl -X POST https://flomoapp.com/iwh/NDA3OTAy/68a69fc8ee7b2508030e9a223dc49eb4/ -H "Content-type: application/json" -d '{"content": "#readwise \n If you’re not the driver, then you’ll always be at the whim of everyone else’s travel plans.” -- xxxx - &lt;yyyyy&gt;" }' 
