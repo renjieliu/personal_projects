71ec7f67ef3c691801e802b7a85999a4933cb4f9
renjieliu <anlrj@qq.com>
2024-12-02 15:34:12 -0500

    .

--- google_photos/find_the_ones_not_uploaded.py
+++ google_photos/find_the_ones_not_uploaded.py
@@ -2,7 +2,7 @@ local = set()
 cloud = set()
 with open("local.txt", encoding='UTF-8') as f: 
     for line in f:
-        curr = line.split('\\')[-1]
+        curr = line.strip('"').split('\\')[-1] # take out the double quote at the start and end of the path
         local.add(curr)
 
 with open("output_google_photos.txt", encoding='UTF-8') as f:
