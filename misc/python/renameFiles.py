0887191b25220762f0fe154dfa103448c08855b0
Renjie Liu <anlrj@qq.com>
2024-12-24 17:25:34 -0500

    .

--- misc/python/renameFiles.py
+++ misc/python/renameFiles.py
@@ -1,10 +1,11 @@
 import os
 
-path = r"C:\Users\liurenjie\Desktop\New folder\\"
+path = r"C:\Users\liurenjie\Desktop\Assets\\"
 
 for f in os.listdir(path):
     file = path +f
-    os.rename(file, file+".jpg")
+    rename_into = file+".jpg"
+    os.rename(file, rename_into)
 
 
 
