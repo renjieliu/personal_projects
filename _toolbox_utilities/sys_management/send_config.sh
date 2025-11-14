4acec7015c73ce19280dec282eeb3d4e7aea2616
Renjie Liu <anlrj@qq.com>
2025-11-14 10:55:56 -0500

    personal_projects/_toolbox_utilities/sys_management/send_config.sh

--- _toolbox_utilities/sys_management/send_config.sh
+++ _toolbox_utilities/sys_management/send_config.sh
@@ -3,7 +3,7 @@
 set -e
 
 cd /home/pi/Share/mail_config
-touch 1.zip
+touch 1.zip # this is a dummy file to make sure below command is working
 rm /home/pi/Share/mail_config/*.zip
 zip -P xxxxxxxxx config_prod.zip  /home/pi/.config/rclone/rclone.conf /home/pi/.bashrc /home/pi/.nanorc
 echo "" | mail -A /home/pi/Share/mail_config/config_prod.zip -s "$(hostname): Message from $(hostname), Here's my config file on $(hostname)" xxxxx@xxxxxxx.com
