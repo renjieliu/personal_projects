f9646d215c0dfc7e3951e4b618ce0228c523a47e
Renjie Liu <anlrj@qq.com>
2025-12-06 20:43:38 -0500

    _toolbox_utilities/download_my_github_repositories.sh

---
+++ _toolbox_utilities/download_my_github_repositories.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir ./renjieliu
+cd ./renjieliu
+
+git clone https://github.com/renjieliu/jot.git
+git clone https://github.com/renjieliu/sql_aoc_2025.git
+git clone https://github.com/renjieliu/leetcode.git
+git clone https://github.com/renjieliu/personal_projects.git
+git clone https://github.com/renjieliu/renjieliu.github.io.git
+git clone https://github.com/renjieliu/databricks-fraud-lakehouse.git
+git clone https://github.com/renjieliu/raspberrypi.git
+git clone https://github.com/renjieliu/sql-playground.git
+git clone https://github.com/renjieliu/sql-ascii-art.git
+git clone https://github.com/renjieliu/sql-barcode.git
+
